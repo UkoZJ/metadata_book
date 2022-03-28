6c229a842424f8e32416f2d813025607b751a1fa
Zivko Juznic <jzivko@gmail.com>
2022-03-28 16:19:33 +0200

    Updated toc

--- build_book.sh
+++
@@ -1,1 +0,0 @@
-jupyter book build /home/uko/Dev/metadata/ --path-output /home/uko/Dev/metadata_book/ --toc /home/uko/Dev/metadata_book/_toc.yml
--- build_book_ma.sh
+++ build_book_ma.sh
@@ -1,3 +1,6 @@
 jupyter book build /home/uko/Dev/metadata/ --path-output /home/uko/Dev/metadata_book/ --toc /home/uko/Dev/metadata_book/_toc.yml
 python /home/uko/Dev/metadata/json2head.py
 python /home/uko/Dev/metadata/meta_collapse.py
+
+# Copy google verification file to _build
+cp googled194e0c154256c20.html ./_build/html
