807b9cd0dcb9ba0c03c8dcd59446f58185f3ee4a
Zivko Juznic <jzivko@gmail.com>
2022-04-01 11:45:50 +0200

    Minimal conda requirements

--- build_book_ma.sh
+++ build_book_ma.sh
@@ -1,6 +1,9 @@
-jupyter book build /home/uko/Dev/metadata/ --path-output /home/uko/Dev/metadata_book/ --toc /home/uko/Dev/metadata_book/_toc.yml
-python /home/uko/Dev/metadata/json2head.py
-python /home/uko/Dev/metadata/meta_collapse.py
+source config.sh
+jupyter book build $PATH_DEV/metadata/ --path-output $PATH_DEV/metadata_book/ --toc $PATH_DEV/metadata_book/_toc.yml
+python $PATH_DEV/metadata/json2head.py
+python $PATH_DEV/metadata/meta_collapse.py
 
 # Copy google verification file to _build
 cp googled194e0c154256c20.html ./_build/html
+cp environment.yml ./_build/html
+cp -r $PATH_DEV/metadata/notebooks/src ./_build/html/_sources/notebooks
