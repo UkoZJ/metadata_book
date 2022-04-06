d57a75d65daabdca5d3df0e6c68e43e69d413160
Zivko Juznic <jzivko@gmail.com>
2022-04-06 13:55:14 +0200

    Remove not available code-example pages from toc

--- build_book_ma.sh
+++ build_book_ma.sh
@@ -1,9 +1,24 @@
 source config.sh
+
+# Remove the old version of the website
+if [ -d "./_build" ] 
+then
+    echo "Remove old website version"
+    rm -r _build 
+fi
+
+# Build the jupyter book
 jupyter book build $PATH_DEV/metadata/ --path-output $PATH_DEV/metadata_book/ --toc $PATH_DEV/metadata_book/_toc.yml
+
+# Add schema.org metadata to header
 python $PATH_DEV/metadata/json2head.py
+
+# Modify generated metadata tables (collapse, style, etc.)
 python $PATH_DEV/metadata/meta_collapse.py
 
-# Copy google verification file to _build
+# Add google website index verification file 
 cp googled194e0c154256c20.html ./_build/html
+
+# Add conda requirementes, notebooks and utility scripts to build and use Binder
 cp environment.yml ./_build/html
 cp -r $PATH_DEV/metadata/notebooks/src ./_build/html/_sources/notebooks
